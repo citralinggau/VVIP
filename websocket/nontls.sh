09217d58aafbeab5052f9507e9b00e5e40e8d1bd
citralinggau <128672348+citralinggau@users.noreply.github.com>
2023-05-13 18:48:29 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,7 +16,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by JarStoreVPN " | lolcat
+echo "Installing SSH Websocket None TLS by JarwoksStoreVPN " | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
